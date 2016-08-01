1bec0de2c965b2bf5e4aa36d4f97711531637faf
Roman <w.romankornev@gmail.com>
2016-08-01 23:51:56 +0300

    [INIT] Initial commit

---
+++ bots.py
@@ -0,0 +1,27 @@
+import requests
+import click
+import sys
+
+
+@click.command()
+@click.argument('channel')
+def vbots(channel):
+    response = requests.get('https://api.twitch.tv/kraken/streams/%s' % channel).json()
+    if 'message' in response:
+        print(response['message'])
+        return
+    else:
+        stream = response.get('stream', {})
+        if stream:
+            viewers = stream.get('viewers', 0)
+        else:
+            print("Channel '{}' is offline".format(channel))
+            return
+    chatters = requests.get('https://tmi.twitch.tv/group/user/%s/chatters' % channel).json()['chatter_count']
+    print('Viewers: {}'.format(viewers),
+          'In chat: {}'.format(chatters),
+          'Bots: {}'.format(viewers - chatters),
+          'Bot %: {:.2f}%'.format((viewers - chatters)/viewers*100), sep='\n')
+
+if __name__ == '__main__':
+    vbots()
